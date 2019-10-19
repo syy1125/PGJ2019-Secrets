590d8dd692a1376494efcd609e3b8086dc1e0ade
syy1125 <syy1125@hotmail.com>
2019-10-19 11:24:34 -0700

    Button now makes sound when clicked

--- Assets/Scripts/ButtonControl.cs
+++ Assets/Scripts/ButtonControl.cs
@@ -1,3 +1,4 @@
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -5,17 +6,23 @@ public class ButtonControl : MonoBehaviour
 {
 	[Header("References")]
 	public Transform PlayerHeadTransform;
-
 	public Animator Animator;
+	public AudioSource Audio;
+
+	[Header("Config")]
+	public float SoundDuration;
+	public AnimationCurve VolumeCurve;
 
 	private static readonly int Focused = Animator.StringToHash("Focused");
 
 	public UnityEvent OnClick;
 	private bool _hasAnimator;
+	private bool _played;
 
 	private void Start()
 	{
 		_hasAnimator = Animator != null;
+		_played = false;
 	}
 	
 	private void Update()
@@ -38,5 +45,25 @@ public class ButtonControl : MonoBehaviour
 				Animator.SetBool(Focused, false);
 			}
 		}
+	}
+
+	public void PlaySound()
+	{
+		if (_played) return;
+		StartCoroutine(DoPlaySound());
+	}
+
+	private IEnumerator DoPlaySound()
+	{
+		_played = true;
+		
+		float startTime = Time.time;
+		Audio.Play();
+		while ((Time.time - startTime) < SoundDuration)
+		{
+			Audio.volume = VolumeCurve.Evaluate((Time.time - startTime) / SoundDuration);
+			yield return null;
+		}
+		Audio.Stop();
 	}
 }
