900a37593e52c0f7b93477325afb78a035ee1d4a
syy1125 <syy1125@hotmail.com>
2019-10-18 19:43:15 -0700

    Implement basic 3D movement script

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+	public float MinY;
+	public float MaxY;
+
+	private float _angleY;
+
+	private float AngleY
+	{
+		get => _angleY;
+		set => _angleY = Mathf.Clamp(value, MinY, MaxY);
+	}
+
+
+	private void OnEnable()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	private void Update()
+	{
+		AngleY += Input.GetAxisRaw("Mouse Y");
+
+		transform.parent.Rotate(Vector3.up, Input.GetAxisRaw("Mouse X"));
+	}
+
+	private void LateUpdate()
+	{
+		transform.localRotation = Quaternion.Euler(-AngleY, 0f, 0f);
+	}
+
+	private void OnDisable()
+	{
+		Cursor.lockState = CursorLockMode.None;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private Rigidbody _body;
+
+	[Header("Config")]
+	public float Speed;
+
+	private void Start()
+	{
+		_body = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		var input = new Vector3(
+			Input.GetAxisRaw("Horizontal"),
+			0,
+			Input.GetAxis("Vertical")
+		);
+
+		if (input.magnitude > 1)
+		{
+			input.Normalize();
+		}
+		
+		_body.velocity = Speed * transform.TransformVector(input);
+	}
+}
