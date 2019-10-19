647375980945b83eec618bb4ff88ef2fbc62971d
syy1125 <syy1125@hotmail.com>
2019-10-18 20:43:16 -0700

    Implement level 1

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonControl : MonoBehaviour
+{
+	[Header("References")]
+	public Transform PlayerHeadTransform;
+	public Animator Animator;
+
+	private static readonly int Focused = Animator.StringToHash("Focused");
+
+	public UnityEvent OnClick;
+
+	private void Update()
+	{
+		if (Physics.Raycast(PlayerHeadTransform.position, PlayerHeadTransform.forward, out RaycastHit hit))
+		{
+			Animator.SetBool(Focused, hit.collider.gameObject == gameObject);
+			if (Input.GetKeyDown(KeyCode.Mouse0))
+			{
+				OnClick.Invoke();
+			}
+		}
+		else
+		{
+			Animator.SetBool(Focused, false);
+		}
+	}
+}
---
+++ Assets/Scripts/SlidingDoor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SlidingDoor : MonoBehaviour
+{
+	public Vector3 Offset;
+	public float Duration;
+	private Vector3 _start;
+	private Vector3 _end;
+	private bool _run;
+
+	private void Start()
+	{
+		_start = transform.position;
+		_end = _start + Offset;
+		_run = false;
+	}
+
+	public void Slide()
+	{
+		if (_run) return;
+		StartCoroutine(DoSlide());
+	}
+
+	private IEnumerator DoSlide()
+	{
+		_run = true;
+		float startTime = Time.time;
+		while ((Time.time - startTime) < Duration)
+		{
+			transform.position = Vector3.Lerp(_start, _end, (Time.time - startTime) / Duration);
+			yield return null;
+		}
+		transform.position = _end;
+	}
+}
