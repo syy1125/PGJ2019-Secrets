3cd0ce6391516fd257366c6d2cc972b04c4ab565
syy1125 <syy1125@hotmail.com>
2019-10-19 15:12:27 -0700

    Make level 4

---
+++ Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+    [Header("References")]
+    public AudioSource Audio;
+    
+    [Header("Config")]
+    public float SoundDuration;
+    public AnimationCurve VolumeCurve;
+    
+    private bool _played;
+
+    private void Start()
+    {
+        _played = false;
+    }
+
+    public void PlaySound()
+    {
+        if (_played) return;
+        StartCoroutine(DoPlaySound());
+    }
+
+    private IEnumerator DoPlaySound()
+    {
+        _played = true;
+
+        float startTime = Time.time;
+        Audio.Play();
+        while ((Time.time - startTime) < SoundDuration)
+        {
+            Audio.volume = VolumeCurve.Evaluate((Time.time - startTime) / SoundDuration);
+            yield return null;
+        }
+
+        Audio.Stop();
+    }
+}
