86eddeef8ba066cb6a5d477fdfbd609735939fe4
syy1125 <syy1125@hotmail.com>
2019-10-20 11:14:49 -0700

    Start working on checkpoint

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{}
+
+	// Update is called once per frame
+	void Update()
+	{}
+}
